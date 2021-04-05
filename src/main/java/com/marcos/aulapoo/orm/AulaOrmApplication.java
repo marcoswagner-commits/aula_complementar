a49cd51d4ea7940f6ffdd520e64c2c348a13da9e
marcoswagner-commits <marcoswagnersouza@gmail.com>
2021-04-05 20:36:20 -0300

    GET_Disciplina

--- src/main/java/com/marcos/aulapoo/orm/AulaOrmApplication.java
+++ src/main/java/com/marcos/aulapoo/orm/AulaOrmApplication.java
@@ -1,13 +1,34 @@
 package com.marcos.aulapoo.orm;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marcos.aulapoo.orm.model.Disciplina;
+import com.marcos.aulapoo.orm.repository.DisciplinaRepository;
+
 @SpringBootApplication
-public class AulaOrmApplication {
+public class AulaOrmApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaOrmApplication.class, args);
+	}
+
+	@Autowired
+	private DisciplinaRepository disciplinaRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Disciplina d1 = new Disciplina(null, "Programação Orientada a Objetos",64);
+		Disciplina d2 = new Disciplina(null, "Estrutura de Dados",64);
+		Disciplina d3 = new Disciplina(null, "Engenharia de Software",64);
+		
+		disciplinaRepository.saveAll(Arrays.asList(d1,d2,d3));
+		
 	}
 
 }
--- src/main/java/com/marcos/aulapoo/orm/controller/DisciplinaController.java
+++ src/main/java/com/marcos/aulapoo/orm/controller/DisciplinaController.java
@@ -1,16 +1,35 @@
 package com.marcos.aulapoo.orm.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcos.aulapoo.orm.model.Disciplina;
+import com.marcos.aulapoo.orm.repository.DisciplinaRepository;
 
 @RestController
 @RequestMapping(value="/disciplinas")
 public class DisciplinaController {
 	
+	@Autowired
+	private DisciplinaRepository repository;
+	
 	@GetMapping
-	public String listar(){
-		  return "Mensagem em disciplinas";	
+	public List<Disciplina> listar(){
+		List<Disciplina> list = repository.findAll();
+		return list;	
 	}
+	
+	@GetMapping(value="/{id}")
+	public Disciplina listarUm (@PathVariable Integer id){
+		Optional<Disciplina> obj = repository.findById(id);
+		return obj.orElse(null);	
+	}
+	
 
 }
