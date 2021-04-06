367f23f383b799158255afdeb6d9f26dd0cfe99e
marcoswagner-commits <marcoswagnersouza@gmail.com>
2021-04-06 20:31:41 -0300

    alunos_disciplinas-GET-POST-PUT-DELETE

--- src/main/java/com/marcos/aulapoo/orm/controller/AlunoController.java
+++ src/main/java/com/marcos/aulapoo/orm/controller/AlunoController.java
@@ -1,13 +1,20 @@
 package com.marcos.aulapoo.orm.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcos.aulapoo.orm.model.Aluno;
 import com.marcos.aulapoo.orm.repository.AlunoRepository;
@@ -30,5 +37,31 @@ public class AlunoController {
 		Optional<Aluno> obj = repository.findById(id);
 		return obj.orElse(null);	
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> inserir (@RequestBody Aluno obj) {
+		obj = repository.save(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentContextPath().path("{}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> atualizar(@RequestBody Aluno obj, @PathVariable Integer id) {
+		Optional<Aluno> objF = repository.findById(id);
+		objF.get().setNome(obj.getNome());
+		objF.get().setEmail(obj.getEmail());
+		objF.get().setMatricula(obj.getMatricula());
+		objF.get().setCurso(obj.getCurso());
+		obj = repository.save(objF.get());
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Integer id) {
+		repository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 
 }
--- src/main/java/com/marcos/aulapoo/orm/controller/DisciplinaController.java
+++ src/main/java/com/marcos/aulapoo/orm/controller/DisciplinaController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +45,22 @@ public class DisciplinaController {
 				.fromCurrentContextPath().path("{}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> atualizar(@RequestBody Disciplina obj, @PathVariable Integer id) {
+		Optional<Disciplina> objF = repository.findById(id);
+		objF.get().setNome(obj.getNome());
+		objF.get().setCarga_horaria(obj.getCarga_horaria());
+		obj = repository.save(objF.get());
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Integer id) {
+		repository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 	
 
